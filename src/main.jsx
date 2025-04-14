a0fe852cab9bd78a3230413fed4619c6655c9d72
Marina982 <marina.goes@estudante.ifms.edu.br>
2025-04-14 13:05:11 -0300

    save

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import App from './App.jsx';
 import Home from './Home.jsx';
 import AuthMiddleware from './middleware.jsx';
 import Registra from './Components/Registrar.jsx';
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route element={<AuthMiddleware />}>
+        <Route element={<AuthMiddleware/>}>
         <Route path="/" element={<Home/>}/>
         </Route>
         <Route path="/login" element={<App/>} />
